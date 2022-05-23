452836b25a553c750622d58bb073a415089c56cd
Hasibul Hasan Tushar <89701664+HH-Tushar@users.noreply.github.com>
2022-05-23 21:35:56 +0600

    Update lock_thread.c

--- threads/lock_thread.c
+++ threads/lock_thread.c
@@ -1,30 +1,38 @@
 //gcc lock_thread.c -pthread
 /* we use lock and unlock to avoid race condition*/
+
+#include <stdlib.h>
 #include <stdio.h>
 #include <pthread.h>
-int x;
+
+int mails = 0;
 pthread_mutex_t mutex;
-void *routine()
-{
-    for (int i = 0; i < 100000; i++)
-    {
-        pthread_mutex_lock(&mutex);//locking x sothat other thread cannot manupulate it
-        x++;
-        pthread_mutex_unlock(&mutex);//unlocking x sothat other thread can use it again
+
+void* routine() {
+    for (int i = 0; i < 10000000; i++) {
+        pthread_mutex_lock(&mutex);
+        mails++;
+        pthread_mutex_unlock(&mutex);
     }
 }
 
-int main()
-{
-    pthread_t a,b;
-    pthread_mutex_init(&mutex,NULL);
-    pthread_create(&a,NULL,&routine,NULL);
-    pthread_create(&b,NULL,&routine,NULL);
-
-    pthread_join(a,NULL);
-    pthread_join(b,NULL);
-
-
-printf("value of x= %d\n",x);
-return 0;
+int main(int argc, char* argv[]) {
+    pthread_t th[8];
+    int i;
+    pthread_mutex_init(&mutex, NULL);
+    for (i = 0; i < 8; i++) {
+        if (pthread_create(th + i, NULL, &routine, NULL) != 0) {
+            perror("Failed to create thread");
+        }
+        printf("Thread %d has started\n", i);
+    }
+    for (i = 0; i < 8; i++) {
+        if (pthread_join(th + i, NULL) != 0) {
+        }
+        printf("Thread %d has finished execution\n", i);
+    }
+    pthread_mutex_destroy(&mutex); //need to destroy mutux as it is dynamically allocated
+    printf("Number of mails: %d\n", mails);
+    
+    return 0;
 }
