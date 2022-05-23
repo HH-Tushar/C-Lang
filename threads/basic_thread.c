c77198c10a5e0bab08f378ee2e1befef37498390
Hasibul Hasan Tushar <89701664+HH-Tushar@users.noreply.github.com>
2022-05-23 20:48:18 +0600

    Add files via upload

---
+++ threads/basic_thread.c
@@ -0,0 +1,24 @@
+
+/* gcc basic_thread.c -pthread    #must use pthread word */
+#include<stdio.h>
+#include<pthread.h>
+void *run(){
+    printf("1. printing from pthread\n");
+}
+void *run2(){
+    printf("2. printing from pthread\n");
+}
+
+int main(){
+pthread_t p1,p2;
+pthread_create(&p1,NULL,&run,NULL);//create
+pthread_join(p1,NULL);//join with main threat
+
+pthread_create(&p2,NULL,&run,NULL);
+pthread_join(p2,NULL);
+
+/*pthread_create(&p2,NULL,&run2,NULL);
+pthread_join(p2,NULL);*/
+
+    return 0;
+}
